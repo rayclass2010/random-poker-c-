8bbb24283eb47cfc8bc197a85f254c2dfed3ad2f
rayclass2010 <11155048@gs.hs.ntnu.edu.tw>
2024-10-22 16:00:51 +0800

    Add files via upload

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            imageList1 = new ImageList(components);
+            timer1 = new System.Windows.Forms.Timer(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(54, 306);
+            button1.Name = "button1";
+            button1.Size = new Size(154, 61);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(356, 265);
+            button2.Name = "button2";
+            button2.Size = new Size(223, 102);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(73, 61);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 15);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(356, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(42, 15);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(282, 248);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 15);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(73, 79);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(110, 158);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(368, 79);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(110, 158);
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "poker1.jpg");
+            imageList1.Images.SetKeyName(1, "poker2.jpg");
+            imageList1.Images.SetKeyName(2, "poker3.jpg");
+            imageList1.Images.SetKeyName(3, "poker4.jpg");
+            imageList1.Images.SetKeyName(4, "poker5.jpg");
+            imageList1.Images.SetKeyName(5, "poker6.jpg");
+            imageList1.Images.SetKeyName(6, "poker7.jpg");
+            imageList1.Images.SetKeyName(7, "poker8.jpg");
+            imageList1.Images.SetKeyName(8, "poker9.jpg");
+            imageList1.Images.SetKeyName(9, "poker10.jpg");
+            imageList1.Images.SetKeyName(10, "poker11.jpg");
+            imageList1.Images.SetKeyName(11, "poker12.jpg");
+            imageList1.Images.SetKeyName(12, "poker13.jpg");
+            imageList1.Images.SetKeyName(13, "pokerbk.jpg");
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private ImageList imageList1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
